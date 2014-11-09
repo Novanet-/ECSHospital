b6b31443d15cdbb4f6122fb706c9a3a53db11b6a
Novanet- <gamemaster8888@gmail.com>
2014-11-09 17:17:24 +0000

    Created abstract person class

--- ECSHospital/src/Person.java
+++ ECSHospital/src/Person.java
@@ -1,4 +1,32 @@
 
-public class Person {
-	int test1;
+public abstract class Person {
+	char gender;
+	int age;
+	String health;
+	
+	public Person(){
+		this.gender = "F";
+		this.age = "20";
+		this.health = "healthly";
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getHealth() {
+		return health;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public abstract boolean aDayPasses(){
+		
+	}
 }
