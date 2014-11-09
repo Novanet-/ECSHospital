eda7f2516a9d1aace3a7106e1ea435e0eb7bab45
Novanet- <gamemaster8888@gmail.com>
2014-11-09 17:54:43 +0000

    Created Hospital class

---
+++ ECSHospital/src/Hospital.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class Hospital {
+	ArrayList<Boolean> beds = new ArrayList<Boolean>();
+	
+	public int admitPatient(Patient patient){
+		return 0;
+		
+	}
+	
+	public Patient getPatient(int bedIndex){
+		return null;
+		
+	}
+	
+	public int size(){
+		
+	}
+	
+	public void dischargePatient(int bedIndex){
+		
+	}
+	
+	public boolean isTheatreFree(int theatreIndex){
+		
+	}
+	
+	public
+	
+}
